73aeacac7a31b742cabacf1227c3be4f5233b634
Alan Enar <87091720+AlanEnar@users.noreply.github.com>
2023-07-24 17:34:00 -0500

    Try a new method for removeFromArray to account for many arguments

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,10 +1,25 @@
-const removeFromArray = function(array, item1, item2) {
+const removeFromArray = function(array, ...items) {
     let cleanArray = [];
 
-    for (var i = 0; i < array.length; i++){
-        if (array[i] !== item1 && array[i] !== item2){
-            cleanArray.push(array[i]);
+    for (let i = 0; i < array.length; i++){
+        let skip = false;
+
+        for (let j = 0; j < items.length; j++){
+            if (array[i] === items[j]){
+                // If an item in the array to be cleaned matches a black listed item, 
+                // skip that item and go to the next iteration of the loop.
+                skip = true;
+                break;
+            }
         }
+
+        if (skip){
+            // We need the extra variable, because putting the 'continue' in the
+            // j loop wouldn't affect the i loop.
+            continue;
+        }
+
+        cleanArray.push(array[i]);
     }
 
     return cleanArray;
