7585d2ed8126bfe0f10a2c3b0740abb19f310eb4
Alan Enar <87091720+AlanEnar@users.noreply.github.com>
2023-07-24 18:21:36 -0500

    Handle negative parameters

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -4,6 +4,12 @@ const sumAll = function(a, b) {
     let smallNum = a < b ? a : b;
     let bigNum = b > a ? b : a;
 
+    if (smallNum < 0){
+        // We only need to check the small number, because if any negative arguments
+        // are passed, the small number must be negative.
+        return "ERROR";
+    }
+
     let finalSum = 0;
 
     for (var i = smallNum; i <= bigNum; i++){
