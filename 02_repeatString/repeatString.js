57fd4cd0cd908fde5037bd29c90f409dfabfa604
Alan Enar <87091720+AlanEnar@users.noreply.github.com>
2023-07-24 17:11:01 -0500

    Generate error with negative input

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,10 +1,15 @@
 const repeatString = function(string, num) {
+    // Generate error to prevent infinite loop wiht negative input
+    if (num < 0){
+        return "ERROR";
+    }
+
     let newString = "";
-    newString.concat("testing")
+
     for (var i = num; i--;){
         newString = newString.concat(string);
     }
-    console.log(newString);
+    
     return newString;
 };
 
