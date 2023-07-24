7344cc83622e5b3b71e4be49a7e33d1ad241f5de
Alan Enar <87091720+AlanEnar@users.noreply.github.com>
2023-07-24 17:24:11 -0500

    Create basic functionality for removeFromArray function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, item1, item2) {
+    let cleanArray = [];
 
+    for (var i = 0; i < array.length; i++){
+        if (array[i] !== item1 && array[i] !== item2){
+            cleanArray.push(array[i]);
+        }
+    }
+
+    return cleanArray;
 };
 
 // Do not edit below this line
