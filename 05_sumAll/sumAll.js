6703ee6abe58e9b28c7552571b0449b994c52d1e
Alan Enar <87091720+AlanEnar@users.noreply.github.com>
2023-07-24 18:19:12 -0500

    Create basic functionality for sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    // Use ternary operators to assign biggest to "bigNum" and smallest 
+    // to "smallNum"
+    let smallNum = a < b ? a : b;
+    let bigNum = b > a ? b : a;
 
+    let finalSum = 0;
+
+    for (var i = smallNum; i <= bigNum; i++){
+        finalSum += i;
+    }
+
+    return finalSum;
 };
 
 // Do not edit below this line
